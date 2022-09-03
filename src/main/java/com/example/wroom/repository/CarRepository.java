069b8c110d6db235f602435fc0861ab628823714
Marek Uibo <marekuibo@gmail.com>
2022-09-03 12:34:54 +0300

    author added

--- src/main/java/com/example/wroom/repository/CarRepository.java
+++ src/main/java/com/example/wroom/repository/CarRepository.java
@@ -1,5 +1,7 @@
 package com.example.wroom.repository;
-
+/**
+ * @author:Marek Uibo
+ */
 import com.example.wroom.models.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/example/wroom/repository/RentalOfficeRepository.java
+++ src/main/java/com/example/wroom/repository/RentalOfficeRepository.java
@@ -1,5 +1,7 @@
 package com.example.wroom.repository;
-
+/**
+ * @author:Marek Uibo
+ */
 import com.example.wroom.models.RentalOffice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
