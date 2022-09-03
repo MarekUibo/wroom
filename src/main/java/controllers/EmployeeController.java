a20d9e5bb2c9e67da44a2ae1f98432069c876e71
Marek Uibo <marekuibo@gmail.com>
2022-09-03 13:20:02 +0300

    controllers added

---
+++ src/main/java/controllers/EmployeeController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author:Marek Uibo
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+}
