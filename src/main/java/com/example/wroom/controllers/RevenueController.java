fd1dbade6f1814ffb3353b26e40bf69d77666735
Marek Uibo <marekuibo@gmail.com>
2022-09-03 13:29:19 +0300

    controller java doc added

--- src/main/java/com/example/wroom/controllers/RevenueController.java
+++ src/main/java/com/example/wroom/controllers/RevenueController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author:Marek Uibo
+ * Controller to handle requests for revenue
+ *
+ * @author Marek Uibo
  */
 @Controller
 @RequestMapping("/revenue")
