103275cf74380c4937399dbdb4a511835a6389b6
Jonathan Rigottier <jrigottier@gmail.com>
2022-09-04 12:39:47 +0300

    Person service implementation added

--- src/main/java/com/example/wroom/services/implementations/PersonServiceImpl.java
+++ src/main/java/com/example/wroom/services/implementations/PersonServiceImpl.java
@@ -2,14 +2,14 @@ package com.example.wroom.services.implementations;
 
 import com.example.wroom.exceptions.PersonNotFoundException;
 import com.example.wroom.models.Person;
-import com.example.wroom.repository.BookingRepository;
-import com.example.wroom.repository.EmployeeRepository;
+import com.example.wroom.repository.PersonRepository;
 import com.example.wroom.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -22,40 +22,57 @@ import java.util.UUID;
 public class PersonServiceImpl implements PersonService {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private PersonRepository personRepository;
 
     @Override
     public void createPerson(Person person) {
-
+        person.setActive(true);
+        personRepository.save(person);
     }
 
     @Override
     public Person findPersonById(UUID id) throws PersonNotFoundException {
-        return null;
+        Optional<Person> optionalPerson = personRepository.findById(id);
+
+        if(optionalPerson.isEmpty()) {
+            throw new PersonNotFoundException(id);
+        }
+        return optionalPerson.get();
     }
 
     @Override
     public Person findPersonByEmail(String email) throws PersonNotFoundException {
-        return null;
+        Optional<Person> optionalPerson = personRepository.findByEmail(email);
+
+        if(optionalPerson.isEmpty()) {
+            throw new PersonNotFoundException(email);
+        }
+        return optionalPerson.get();
     }
 
     @Override
     public List<Person> findAllPersons() {
-        return null;
+        return personRepository.findAll();
     }
 
     @Override
     public void updatePerson(Person person) throws PersonNotFoundException {
-
+        if(findPersonById(person.getId()) != null) {
+            personRepository.saveAndFlush(person);
+        }
     }
 
     @Override
     public void deletePersonById(UUID id) throws PersonNotFoundException {
-
+        Person person = findPersonById(id);
+        person.setActive(false);
+        personRepository.saveAndFlush(person);
     }
 
     @Override
     public void restorePersonById(UUID id) throws PersonNotFoundException {
-
+        Person person = findPersonById(id);
+        person.setActive(true);
+        personRepository.saveAndFlush(person);
     }
 }
