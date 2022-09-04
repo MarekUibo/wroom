9f4b8ed0e821b5426328a401f08b8da1fee9c529
Marek Uibo <marekuibo@gmail.com>
2022-09-04 09:35:33 +0300

    name changed for rental/return

--- src/main/java/com/example/wroom/controllers/ReturnController.java
+++ src/main/java/com/example/wroom/controllers/ReturnController.java
@@ -3,7 +3,7 @@ package com.example.wroom.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
+/** Controller to handle return requests
  * @author:Marek Uibo
  */
 @Controller
